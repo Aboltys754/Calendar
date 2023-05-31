9d5becd2afe866b544eff8e3b21d8f8eaa5c8f4d
Aboltys754 <warwar754@mail.ru>
2023-05-31 08:10:59 +0500

    ...

--- src/components/Calendar/CalendarForm/FooterForm/FooterForm.tsx
+++ src/components/Calendar/CalendarForm/FooterForm/FooterForm.tsx
@@ -11,16 +11,22 @@ type TypeCalendarForm = {
 }
 
 export default function FooterForm({date, setDate, month, setMonth, year, setYear}: TypeCalendarForm) {
-    const newDate = new Date();
+    const newDay = new Date(year, month, 1).getDay()
     const amountOfDays = new Date(year, month + 1, 0).getDate()
-    const [day, setDay] = useState(newDate.getDay());
-    const arraydate = new Array(7).fill(0)
+    // const [day, setDay] = useState(newDate);
+    const arraydate = new Array(42).fill(0)
+    arraydate.fill(null, newDay - 1, amountOfDays + newDay - 1)
     console.log(arraydate)
+    // console.log(day)
     return (
         <div className={styles.root}>
-            {new Array(amountOfDays).fill(null).map((_, index) => (
-                <div key={index} className={styles.date}>{index + 1}</div>
+            {arraydate.map((value, index) => (
+                value === 0 ? <div key={index} className={styles.date}>{` `}</div> : <div key={index} className={styles.date}>{index + 2 - newDay}</div>
             ))}
+
+            {/* {new Array(amountOfDays).fill(null).map((_, index) => (
+                <div key={index} className={styles.date}>{index + 1}</div>
+            ))} */}
         </div>
     )
 }
