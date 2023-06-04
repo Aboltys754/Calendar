0ab8f30a34c269f4e68b7d9a60aefbf4b5ac374a
Aboltys754 <warwar754@mail.ru>
2023-06-04 10:05:43 +0500

    for

--- src/components/Calendar/CalendarForm/FooterForm/FooterForm.tsx
+++ src/components/Calendar/CalendarForm/FooterForm/FooterForm.tsx
@@ -11,46 +11,70 @@ type TypeCalendarForm = {
 }
 
 export default function FooterForm({date, setDate, month, setMonth, year, setYear}: TypeCalendarForm) {
-    // const lastDay = new Date(year, month - 1, 0).getDate()
-    // const newData = new Date(year, month, 0).getDate()
-    // const newDay = new Date(year, month, 0).getDay()
-    // const arraydate = new Array(42).fill(null)
+    // дней в предыдущем месяце
+    const lastData = new Date(year, month, 0).getDate()
+    // дней в нынешнем месяце
+    const newData = new Date(year, month + 1, 0).getDate()
+    // с какого дня недели начинается нынешний месяц
+    const newDay = new Date(year, month, 0).getDay()
+    const arraydate = [] as number[]
     // arraydate.fill(0, 0, newDay);
     // arraydate.fill(1, newDay, newData);
 
-    // console .log(lastDay);
-    // console .log(newDay);
-    // console .log(arraydate);
+    for (let i = lastData - newDay + 1; i <= lastData; i++) {
+        arraydate.push(i)
+    }
 
-    // return (
-    //     <div>
-    //         1
-    //     </div>
-    // )
+    for (let i = 1; i <= newData; i++) {
+        arraydate.push(i)
+    }
 
+    console.log(37 - arraydate.length);
+    console.log(arraydate.length);
 
+    if (arraydate.length < 36) {
+        for (let i = 1; i < 37 - arraydate.length; i++) {
+            arraydate.push(i)
+        }
+    }
 
+    // console.log(lastData);
+    // console.log(newData);
+    // console.log(newDay);
+    // console.log(36 - arraydate.length);
+    console.log(arraydate);
 
-
-
-
-
-
-
-
-    const lastDay = new Date(year, month - 1, 1).getDay()
-    const newDay = new Date(year, month, 1).getDay()
-    const amountOfDays = new Date(year, month + 1, 0).getDate()
-    // const [day, setDay] = useState(newDate);
-    const arraydate = new Array(42).fill(0)
-    arraydate.fill(null, newDay - 1, amountOfDays + newDay - 1)
-    console.log(arraydate)
-    console.log(lastDay)
     return (
         <div className={styles.root}>
             {arraydate.map((value, index) => (
-                value === 0 ? <div key={index} className={styles.date}>{` `}</div> : <div key={index} className={styles.date}>{index + 2 - newDay}</div>
+                <div key={index} className={styles.date}>{value}</div>
             ))}
         </div>
     )
+
+
+
+
+
+
+
+
+
+
+
+    // const lastDay = new Date(year, month - 1, 1).getDay()
+    // const newDay = new Date(year, month, 1).getDay()
+    // const amountOfDays = new Date(year, month + 1, 0).getDate()
+    // // const [day, setDay] = useState(newDate);
+    // const arraydate = new Array(42).fill(0)
+    // arraydate.fill(null, newDay - 1, amountOfDays + newDay - 1)
+    // console.log(arraydate)
+    // console.log(lastDay)
+    // return (
+    //     <div className={styles.root}>
+    //         {arraydate.map((value, index) => (
+    //             value === 0 ? <div key={index} className={styles.date}>{` `}</div> : <div key={index} className={styles.date}>{index + 2 - newDay}</div>
+    //         ))}
+    //     </div>
+    // )
 }
